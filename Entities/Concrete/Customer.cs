4e04b4faa7e2cc9405b76a755146e5e07f881a17
osmansaits <63861402+osmansaits@users.noreply.github.com>
2021-03-20 23:06:11 +0300

    Add project files.

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Customer : IEntity
+    {   //CustomerId veri tabanında string olduğu için string kodlanmıştır.
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }          
+    }
+}
